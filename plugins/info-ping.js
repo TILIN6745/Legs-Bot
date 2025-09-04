fad8a2d7ffd9cc9c66e34ced03386b5dfe239941
GianPoolS <116328472+GianPoolS@users.noreply.github.com>
2025-09-04 08:11:38 -0500

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
                   name: 'quick_reply',
                   buttonParamsJson: JSON.stringify({
                     display_text: '⚡ Ver velocidad',
-                    id: `${usedPrefix}speed2`
+                    id: `${usedPrefix}speed`
                   })
                 },
                 {
