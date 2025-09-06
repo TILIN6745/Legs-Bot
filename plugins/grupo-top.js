b45100c5b9a5697090e5917b325e2758f4b93c67
Ado <minexdt@gmail.com>
2025-09-06 07:52:42 -0600

    Actualizar grupo-top.js

--- plugins/grupo-top.js
+++ plugins/grupo-top.js
@@ -2,35 +2,35 @@ import path from 'path'
 let user = a => '@' + a.split('@')[0]
 
 function handler(m, { groupMetadata, command, conn, text }) {
-  if (!text) return conn.reply(m.chat, `📊 ¿Top de qué?\n\nEjemplo: *${command} usuarios más activos*`, m)
+  if (!text) return conn.reply(m.chat, `✩ ¿Qué ranking quieres ver?\n\nEjemplo: *${command} usuarios más activos*`, m)
 
   let ps = groupMetadata.participants.map(v => v.id)
   let [a, b, c, d, e, f, g, h, i, j] = Array.from({ length: 10 }, () => ps.getRandom())
 
   let intro = pickRandom([
-    `📊 *Top 10 de ${text.toUpperCase()}*`,
-    `🏆 *Ranking de ${text}*`,
-    `⭐ *Lista de los más destacados en ${text}*`,
+    `> ✧ *Top 10 de ${text.toUpperCase()}*`,
+    `> ✩ *Ranking oficial de ${text}*`,
+    `> ✐ *Los más destacados en ${text}*`
   ])
 
   let top = 
 `${intro}
 
-1. 🥇 ${user(a)}
-2. 🥈 ${user(b)}
-3. 🥉 ${user(c)}
-4. 🎖️ ${user(d)}
-5. ✨ ${user(e)}
-6. 📌 ${user(f)}
-7. 📈 ${user(g)}
-8. 🔰 ${user(h)}
-9. 🎯 ${user(i)}
-10. 📍 ${user(j)}`
+❐ ${user(a)}
+✩ ${user(b)}
+✧ ${user(c)}
+ꕥ ${user(d)}
+✐ ${user(e)}
+❐ ${user(f)}
+✩ ${user(g)}
+✧ ${user(h)}
+ꕥ ${user(i)}
+✐ ${user(j)}`
 
   conn.reply(m.chat, top, m, { mentions: [a, b, c, d, e, f, g, h, i, j] })
 }
 
-handler.help = ['top *<texto>*']
+handler.help = ['top']
 handler.command = ['top']
 handler.tags = ['group']
 handler.group = true
